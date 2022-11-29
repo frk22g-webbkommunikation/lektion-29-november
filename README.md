b8e47418e9c2460a207a06de444dda6518b3ce9a
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-11-29 15:12:50 +0100

    Added code

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Skapa ett Firebaseprojekt och konfigurera upp er applikation så den ansluts mot
 
 Tutorial: https://firebase.google.com/docs/web/setup?authuser=0&hl=en
 
-Sedan ska du skapa en webbsida där man kan se och lägga till Shakespeare insults som ska sparas i din Firestore databas. Det du ska sparas ska vara ett objekt med två egenskaper `insult` och `play`. Förolämpningarna ska läggas till av en användaren via inputfält.
+Sedan ska du skapa en webbsida där man kan se och lägga till Shakespeare insults som ska sparas i din Firestore databas. Det du ska sparas ska vara ett objekt med två egenskaper `insult` och `play`. Förolämpningarna ska läggas till av en användaren via inputfält. 
 
 Ex:
 ```
@@ -17,3 +17,20 @@ Ex:
 ```
 
 Shakespeare insult hittar du här: https://gist.github.com/zocom-christoffer-wallenberg/891d305bc8fa9f000885f409f2ec399b
+
+**Veckans övning**
+
+Du ska fortsätta spelet sten, sax och påse (det finns det i detta repo o i mappen `rock-paper-scissors`) och lägga till en highscore-lista som ska sparas i en firestore databas. 
+Spelet spelas i omgångar av 3 och efter tre matcher så ska användarens statistik sparas i din firestore databas tillsammans med ett användarnamn som användaren får mata in i ett inputfält.
+
+Såhär ska det se ut i din databas när du har sparat (Number och String menas att det ska vara ett värde av typen nummer eller en sträng).
+```javascript
+{
+    wins: Number,
+    loses: Number,
+    draws: Number,
+    username: String
+}
+```
+
+Det ska även på din sida gå och highscorelistan.
---
+++ rock-paper-scissors/js/index.js
@@ -0,0 +1,104 @@
+//Hämta användarens val
+
+//Slumpa datorns val
+
+//Avgör vinnare
+
+//Visa vinnare
+
+let userWeapon; //Här sparar vi valet som användaren gör
+let computerWeapon; //Här sparar vi datorns val
+let matches = 0;
+let stats = {
+    wins: 0,
+    loses: 0,
+    draws: 0
+}
+
+//Det gör här är att vi hämtar knappen och kör kod när användaren klickar på knappen.
+document.getElementById('rock').addEventListener('click', function() {
+    userWeapon = 'sten';
+    console.log(`Du har valt: ${userWeapon}`);
+    getWinner();
+});
+
+document.getElementById('scissor').addEventListener('click', function() {
+    userWeapon = 'sax';
+    console.log(`Du har valt: ${userWeapon}`);
+    getWinner();
+});
+
+document.getElementById('paper').addEventListener('click', function() {
+    userWeapon = 'påse';
+    console.log(`Du har valt: ${userWeapon}`);
+    getWinner();
+});
+
+function resetGame() {
+    matches = 0;
+    stats = {
+        wins: 0,
+        loses: 0,
+        draws: 0
+    }
+}
+
+function showWinner(winner) {
+    document.getElementById('winner').innerHTML = 'Vinnare avgörs...';
+    matches++;
+    setTimeout(() => {
+        if (matches === 3) {
+            document.getElementById('winner').innerHTML = `
+                <p>Vinster: ${stats.wins}</p>
+                <p>Förluster: ${stats.loses}</p>
+                <p>Oavgjort: ${stats.draws}</p>
+            `
+        } else {
+            document.getElementById('winner').innerHTML = winner;
+        }
+    }, 2000) // Kör denna funktion efter 2s
+}
+
+function getWinner() {
+    computerWeapon = Math.round(Math.random() * 2); //Slumpa ett nummer mellan 0-2.
+
+    if (computerWeapon == 0) {
+        computerWeapon = 'sten';
+    } else if(computerWeapon == 1) {
+        computerWeapon = 'sax';
+    } else if(computerWeapon == 2) {
+        computerWeapon = 'påse';
+    }
+
+    console.log(`Ditt vapen: ${userWeapon} Datorns vapen: ${computerWeapon}`);
+
+    //Avgör vinnare. Kollar först om det blir blivit oavgjort annars vem som vann.
+    if (userWeapon == computerWeapon) {
+        stats.draws++;
+        showWinner('Det blev oavgjort');
+    } else if (userWeapon == 'sten') {
+        if (computerWeapon == 'sax') {
+            stats.wins++;
+            showWinner('Du vann!');
+        } else {
+            stats.loses++;
+            showWinner('Datorn vann!');
+        }
+    } else if (userWeapon == 'sax') {
+        if (computerWeapon == 'påse') {
+            stats.wins++;
+            showWinner('Du vann!');
+        } else {
+            stats.loses++;
+            showWinner('Datorn vann!');
+        }
+    } else if (userWeapon == 'påse') {
+        if (computerWeapon == 'sten') {
+            stats.wins++;
+            showWinner('Du vann!');
+        } else {
+            stats.loses++;
+            showWinner('Datorn vann!');
+        }
+    }
+}
